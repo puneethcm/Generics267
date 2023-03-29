a29a26a22045cfaa662f5af6213836b9975ed6e5
puneethcm <puneethkumarcm8050@gmail.com>
2023-03-29 18:06:03 +0530

    modified

--- Generic267Batch/FindIntegerMinmum.cs
+++ Generic267Batch/FindIntegerMinmum.cs
@@ -19,6 +19,42 @@ namespace Generic267Batch
             }
             throw new Exception("2 or 3 value or same");
         }
+
+        public static float FindFloatMinValues(float firstNumb, float secondNumb, float thirdNumb)
+        {
+            if (firstNumb.CompareTo(secondNumb) < 0 && firstNumb.CompareTo(thirdNumb) < 0)
+            {
+                return firstNumb;
+            }
+            else if (secondNumb.CompareTo(firstNumb) < 0 && secondNumb.CompareTo(thirdNumb) < 0)
+            {
+                return secondNumb;
+            }
+            else if (thirdNumb.CompareTo(firstNumb) < 0 && thirdNumb.CompareTo(secondNumb) < 0)
+            {
+                return thirdNumb;
+            }
+            throw new Exception("2 or 3 value or same");
+        }
+
+        public static string FindStringMinValues(string firstNumb, string secondNumb, string thirdNumb)
+        {
+            if (firstNumb.CompareTo(secondNumb) < 0 && firstNumb.CompareTo(thirdNumb) < 0)
+            {
+                return firstNumb;
+            }
+            else if (secondNumb.CompareTo(firstNumb) < 0 && secondNumb.CompareTo(thirdNumb) < 0)
+            {
+                return secondNumb;
+            }
+            else if (thirdNumb.CompareTo(firstNumb) < 0 && thirdNumb.CompareTo(secondNumb) < 0)
+            {
+                return thirdNumb;
+            }
+            throw new Exception("2 or 3 value or same");
+        }
+
+
     }
 }
 
